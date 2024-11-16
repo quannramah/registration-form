4aa6dca61a7fbded0fabb16ad8f6e14d6fd8f986
ramadhani mwanyota <108824725+quannramah@users.noreply.github.com>
2024-11-16 22:28:01 +0300

    Add files via upload

---
+++ OOPCAT2/src/main/java/com/mycompany/oopcat2/OOPCAT2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopcat2;
+
+/**
+ *
+ * @author user
+ */
+public class OOPCAT2 {
+
+    public static <String> void main(String[] args) {System.out.println("Hello World!");
+    }
+}
